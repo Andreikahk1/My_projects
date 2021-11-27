771ecd85802a47bf9e1426f3761bf4f07c8ee336
Andreikahk1 <andreikahk@tut.by>
2021-11-27 19:18:49 +0300

    My HW to Lecture 7

---
+++ program_hw_mysplit/progs/Andrey_Popok_7.1.py
@@ -0,0 +1,7 @@
+from program_hw_mysplit.packages.mysplit.mysplit import mysplit
+
+print(mysplit("To be or not to be, that is the question"))
+print(mysplit("To be or not to be,that is the question"))
+print(mysplit("     "))
+print(mysplit(" abc "))
+print(mysplit(""))
